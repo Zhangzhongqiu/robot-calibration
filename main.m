5954dcffbda1ea0e4e77f9cf774af0c35ec81744
zhangz <zhangzhongqiu1992@gmail.com>
2017-01-04 17:40:01 +0100

    Add main.m

---
+++ main.m
@@ -0,0 +1,16 @@
+%read the output of measurement
+data=csvread('20161118_run_1.csv',21,1);
+[Q,tau,Tr2]=gettrans(data);
+%calibration
+[x,fval, exitflag, output]=opt21(Q,tau,Tr2);
+save('outputs.mat','x');  %%save the identified errors
+%error calculation
+[q_opt,q_test]=qget(Q);
+[Tc1,Tc2]=coordinate_trans(Q,Tr2);
+% load Tc1;%load coordinate transform matrix before calibration Tc1
+% load Tc2;%load coordinate transform matrix before calibration Tc2
+[Tc1_ac,Tc2_ac]=gettc(x);% get the coordinate transform matrix after calibration
+[theta,d]=error_est(x,tau,q_opt,Tr2,Tc1,Tc2,Tc1_ac,Tc2_ac);
+
+%figure plot
+[f1,f2]=figplot(d,theta);
